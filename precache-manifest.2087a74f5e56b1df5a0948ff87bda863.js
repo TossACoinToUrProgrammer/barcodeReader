541c63f39f4d133dbfa9621c0a18db21c597cf72
Arlen Manasov <59771669+TossACoinToUrProgrammer@users.noreply.github.com>
2020-07-16 21:29:48 +0600

    Updates

---
+++ precache-manifest.2087a74f5e56b1df5a0948ff87bda863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffea48c5f3eb6cad61afef98a88ad290",
+    "url": "/barcodeReader/index.html"
+  },
+  {
+    "revision": "9343586b197399a518a4",
+    "url": "/barcodeReader/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2ddb84e2b7d31bb7f20",
+    "url": "/barcodeReader/static/js/2.33995efa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barcodeReader/static/js/2.33995efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9343586b197399a518a4",
+    "url": "/barcodeReader/static/js/main.958b453f.chunk.js"
+  },
+  {
+    "revision": "23f69c6787c30db7a553",
+    "url": "/barcodeReader/static/js/runtime-main.80f69101.js"
+  }
+]);
